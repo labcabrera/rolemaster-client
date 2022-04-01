c18dc7965fb6452f4dc0659248049de07298409d
Luis Cabrera <lab.cabrera@gmail.com>
2022-04-01 20:10:38 +0200

    Initial version

--- src/app/pages/tactical-session/tactical-session.component.ts
+++ src/app/pages/tactical-session/tactical-session.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { TacticalSession } from 'src/app/model/session';
 import { TacticalSessionsService } from 'src/app/services/tactical-sessions.service';
@@ -11,10 +12,17 @@ import { TacticalSessionsService } from 'src/app/services/tactical-sessions.serv
 export class TacticalSessionComponent implements OnInit {
 
   tacticalSession: TacticalSession = {} as TacticalSession;
+  form: FormGroup;
 
   constructor(
     private route: ActivatedRoute,
-    private tacticalSessionService: TacticalSessionsService) { }
+    private tacticalSessionService: TacticalSessionsService,
+    private fb: FormBuilder) {
+      this.form = fb.group({
+        name: ['', Validators.required],
+        description: [''],
+      });
+    }
 
   ngOnInit(): void {
     const id = String(this.route.snapshot.paramMap.get('id'));
