8f888939b7d02b20945b04458f156de04a08777a
Luis Cabrera <lab.cabrera@gmail.com>
2022-04-02 08:54:18 +0200

    Initial version

--- src/app/model/spell.ts
+++ src/app/model/spell.ts
@@ -1,11 +1,25 @@
 export interface Spell {
     id: string,
     name: string,
+    spellListId: string,
     level: number;
+    type: string;
+    subtype: string;
+    preparation: string;
+    targetType: string;
+    targetModifier: string;
+    durationType: string;
+    durationModifier: string;
+    rangeType: string;
+    rangeModifier: string;
+    ppMultiplier: number;
     description: string;
 }
 
 export interface SpellList {
     id: string;
     name: string;
+    realm: string;
+    type: string;
+    description: string;
 }
