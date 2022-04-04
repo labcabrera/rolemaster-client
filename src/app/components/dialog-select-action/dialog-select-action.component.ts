72af2904292d38b2b7bf610cbdadbb3dfe8fadd0
Luis Cabrera <lab.cabrera@gmail.com>
2022-04-04 21:53:14 +0200

    Initial version

--- src/app/components/dialog-select-action/dialog-select-action.component.ts
+++ src/app/components/dialog-select-action/dialog-select-action.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NamedKey } from 'src/app/model/commons';
+import { EnumService } from 'src/app/services/enum.service';
 
 @Component({
   selector: 'app-dialog-select-action',
@@ -10,9 +12,16 @@ export class DialogSelectActionComponent implements OnInit {
   minActionPercent = 1;
   maxActionPercent = 100;
 
-  constructor() { }
+  movementPaces: NamedKey[] = [];
+  meleeAttackTypes: NamedKey[] = [];
+
+  constructor(
+    private enumService: EnumService
+  ) { }
 
   ngOnInit(): void {
+    this.enumService.findMovementPaces().subscribe(result => this.movementPaces = result);
+    this.enumService.findMeleeAttackTypes().subscribe(result => this.meleeAttackTypes = result);
   }
 
   setUp(minActionPercent: number, maxActionPercent: number) {
--- src/app/model/commons.ts
+++ src/app/model/commons.ts
@@ -2,3 +2,8 @@ export interface Universe {
     id: string;
     name: string;
 }
+
+export interface NamedKey {
+    key: string;
+    name: string;
+}
